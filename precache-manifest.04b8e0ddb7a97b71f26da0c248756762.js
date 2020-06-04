383e436ece92dc953d0760a9949a938bb3d9bcfe
RedisTheOne <redis123445@gmail.com>
2020-06-04 23:52:40 +0200

    Changes Made

---
+++ precache-manifest.04b8e0ddb7a97b71f26da0c248756762.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "714052af2217425a83c32e6ecc9999d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4dd42238ed9be90c815",
+    "url": "/static/css/main.764a23f1.chunk.css"
+  },
+  {
+    "revision": "21b501f9096c4961abec",
+    "url": "/static/js/2.5f93a36a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5f93a36a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4dd42238ed9be90c815",
+    "url": "/static/js/main.9b5cbc17.chunk.js"
+  },
+  {
+    "revision": "608e1bc1a4ba6cf85865",
+    "url": "/static/js/runtime-main.c9a91e67.js"
+  },
+  {
+    "revision": "990f033ea7833c5f6b5da1c95961fede",
+    "url": "/static/media/add_to_collection.990f033e.png"
+  },
+  {
+    "revision": "9d450f0f66eb9d09956c781fc038ab86",
+    "url": "/static/media/logo.9d450f0f.png"
+  },
+  {
+    "revision": "08ed8052d5f5c5bae912fdefbf6a50c0",
+    "url": "/static/media/not_found.08ed8052.png"
+  },
+  {
+    "revision": "5b39cd1431da135326f2dc6f6693cb0d",
+    "url": "/static/media/remove_from_collection.5b39cd14.png"
+  },
+  {
+    "revision": "255f36b45b7fe8219fd2caad1f044bef",
+    "url": "/static/media/superhero_not_found.255f36b4.png"
+  },
+  {
+    "revision": "8ce8c655be51f7c2211ec2c71470bfcd",
+    "url": "/static/media/superman.8ce8c655.png"
+  },
+  {
+    "revision": "fa700883680503af7607b0fb95a34e81",
+    "url": "/static/media/user.fa700883.png"
+  }
+]);
